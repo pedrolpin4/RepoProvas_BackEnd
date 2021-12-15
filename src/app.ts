d373f3c221f1e0fb053e2fd5b3d7e07ed8e83f1e
Pedro Lucas <pedrolucaspina22@gmail.com>
2021-12-15 14:24:01 -0300

    config: configs heroku's url

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,15 @@
-import "./setup";
-
-import express from "express";
-import cors from "cors";
-import "reflect-metadata";
-
-import connectDatabase from "./database";
-
-import * as userController from "./controllers/userConroller";
+import './setup';
+import express from 'express';
+import cors from 'cors';
+import 'reflect-metadata';
+import connectDatabase from './database';
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/users", userController.getUsers);
-
-export async function init () {
-  await connectDatabase();
+export async function init() {
+    await connectDatabase();
 }
 
 export default app;
